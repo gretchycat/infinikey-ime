615aa23f75725b940c4f099dbee99226585e8d29
Gretchen Maculo <gretchynm@gmail.com>
2026-08-02 01:17:04 -0500

    Configure custom project APK output naming

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,13 @@ android {
     buildFeatures {
         viewBinding = true
     }
+
+    applicationVariants.all {
+        outputs.all {
+            val output = this as com.android.build.gradle.internal.api.BaseVariantOutputImpl
+            output.outputFileName = "infinikey-ime-v${versionName}-${buildType.name}.apk"
+        }
+    }
 }
 
 dependencies {
