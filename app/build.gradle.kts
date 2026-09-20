0e4551081e0649bd5310d88abbda7d68e3bfb6e6
Gretchen Maculo <gretchynm@gmail.com>
2026-09-20 12:29:58 -0500

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "com.infinikey_ime"
         minSdk = 24
         targetSdk = 34
-        versionCode = maxOf(242, gitBuildNumber)
+        versionCode = gitBuildNumber
         versionName = baseVersionName
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
